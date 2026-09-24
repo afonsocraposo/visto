a1ffa9d7f2d0274c7e25b08cbec0d1264b730227
Afonso Raposo <afonsocraposo@gmail.com>
2026-09-24 09:54:44 +0100

    feat: use Mantine UI components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
+import "@mantine/core/styles.css";
 import "./styles.css";
 
 if ("serviceWorker" in navigator) window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js"));
